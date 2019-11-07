df5ef72a3c41889f1930aa9a3fc7f074f59a4bc6
門屋弘毅 <kamonohashiy@gmail.com>
2019-11-07 22:39:15 +0900

    メタ情報を更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,17 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'しまの時刻表',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '日本全国の有人離島の船の時刻表データ(定期航路が存在する場合のみ)を掲載しているサイトです。検索対象はこれから徐々に拡大してく予定。※当サイトと情報を掲載している海運会社等とは、一切関係はありません。' },
+      { name: 'twitter:card', content: 'summary'},
+			{ name: 'twitter:site', content: '@platypus_k86'},
+			{ name: 'og:url', content: 'https://shimajikoku.com/'},
+			{ name: 'og:title', content: 'しまの時刻表'},
+			{ name: 'og:description', content: '日本全国の有人離島の船の時刻表データ(定期航路が存在する場合のみ)を掲載しているサイトです。検索対象はこれから徐々に拡大してく予定。※当サイトと情報を掲載している海運会社等とは、一切関係はありません。' },
+			{ name: 'og:image', content: 'https://shimajikoku.com/image.png'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
